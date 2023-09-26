31e8fbce563da2c3d57880701b1586e3042a3df7
simaomeyrer <simaomeyrer@gmail.com>
2023-09-26 00:56:12 -0300

    fix: spotify logo component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,7 +6,7 @@ import "./style.css"
 export default function Header(props: { withLogo?: boolean }) {
   return (
     <div className="header" style={{ justifyContent: props.withLogo ? "space-between" : "end" }}>
-      {props.withLogo && <SpotifyLogo />}
+      {props.withLogo && <SpotifyLogo color="black" />}
       <Logout />
     </div>
   )
--- src/components/spotify-logo/index.tsx
+++ src/components/spotify-logo/index.tsx
@@ -1,24 +1,26 @@
 import React from "react"
 import spotifyLogoBlack from "../../assets/imgs/Spotify_Logo_RGB_Black.png"
 import spotifyLogoGreen from "../../assets/imgs/Spotify_Logo_RGB_Green.png"
+import spotifyLogoWhite from "../../assets/imgs/Spotify_Logo_RGB_White.png"
 
+type LogoColor = "black" | "green" | "white"
 interface SpotifyLogoProps {
+  color: LogoColor
   position?: "start" | "center" | "end"
-  color?: "black" | "green"
 }
 
 export default function SpotifyLogo(props: SpotifyLogoProps) {
+  const logos: Record<LogoColor, string> = {
+    green: spotifyLogoGreen,
+    black: spotifyLogoBlack,
+    white: spotifyLogoWhite,
+  }
   return (
     <div
       className="logo-container"
       style={{ display: "flex", justifyContent: props.position ?? "start" }}
     >
-      <img
-        className="spotify-logo"
-        src={props.color === "green" ? spotifyLogoGreen : spotifyLogoBlack}
-        width={80}
-        alt="spotify-logo"
-      />
+      <img className="spotify-logo" src={logos[props.color]} width={80} alt="spotify-logo" />
     </div>
   )
 }
