40972be43d0851856add94cbd2d16811b7ca6cbe
simaomeyrer <simaomeyrer@gmail.com>
2023-09-26 00:56:40 -0300

    feat: add suspense

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,20 @@
-import React from "react"
+import React, { Suspense } from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { RecoilRoot } from "recoil"
 import { router } from "./router"
-import Icon from "./components/icon/Icon"
 import Container from "./components/container"
+import LoadingSpinner from "./components/loading-spinner"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
       <Container>
-        <RouterProvider router={router} />
+        <Suspense fallback={<LoadingSpinner />}>
+          <RouterProvider router={router} />
+        </Suspense>
       </Container>
-      <footer className="footer">
-        Trackify
-        <Icon name="speaker" />
-      </footer>
     </RecoilRoot>
   </React.StrictMode>
 )
