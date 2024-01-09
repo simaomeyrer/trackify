6f3db4737a65f85bd85398cf909c365501bc8d40
simaomeyrer <simaomeyrer@gmail.com>
2024-01-09 00:03:34 -0300

    feat: image generator options

--- src/components/image-generator/index.tsx
+++ src/components/image-generator/index.tsx
@@ -5,7 +5,7 @@ export default function ImageGenerator(props: React.PropsWithChildren) {
   const handleGenerateImage = () => {
     const element = document.getElementById("htmlElement")!
 
-    toPng(element).then(function (dataUrl) {
+    toPng(element, { width: 1080, height: 1980 }).then(function (dataUrl) {
       const link = document.createElement("a")
       link.download = "image.png"
       link.href = dataUrl
